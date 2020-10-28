b0ece721f915e04e7ded1f434fc06067f923bb7c
rtperezyera <tllezroberto9@gmail.com>
2020-10-28 15:14:20 -0600

    Generate basic page component in orders

---
+++ src/screens/orders.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import { Navbar } from '../components/navbar';
+
+export const Orders = (props) => {
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <h1>Hello world!</h1>
+      </Container>
+    </>
+  );
+};
