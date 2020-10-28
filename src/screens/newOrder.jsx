e9c89132c51f04549d110a884a59147df9804fb0
rtperezyera <tllezroberto9@gmail.com>
2020-10-28 15:32:08 -0600

    Generate basic page component in NewOrder page

---
+++ src/screens/newOrder.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {  } from 'react-bootstrap';
+
+import { Navbar } from '../components/navbar';
+
+export const NewOrder = (props) => {
+    return (
+        <h1>Hello, React.</h1>
+    );
+}
