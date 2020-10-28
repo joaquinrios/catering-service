ba5b68f793d777fbafe342b2925e13d0ddfc40e8
rtperezyera <tllezroberto9@gmail.com>
2020-10-28 15:40:23 -0600

    Add NewOrder to Routing

--- src/components/routing.jsx
+++ src/components/routing.jsx
@@ -3,12 +3,14 @@ import { Router, Redirect } from '@reach/router'
 
 import { Home } from '../screens/home';
 import { Orders } from '../screens/orders';
+import { NewOrder } from '../screens/newOrder';
 
 export const Routing = ({ navigate }) => {
   return (
     <Router>
       <Home path="/" />
       <Orders path="/orders" />
+      <NewOrder path="newOrder" />
     </Router>
   );
 }
