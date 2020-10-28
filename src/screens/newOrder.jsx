45395cfe846f7af188e317c77889dc86d6b6f060
rtperezyera <tllezroberto9@gmail.com>
2020-10-28 15:38:47 -0600

    Add NewOrder to routing

--- src/screens/newOrder.jsx
+++ src/screens/newOrder.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import {  } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import { Navbar } from '../components/navbar';
 
 export const NewOrder = (props) => {
-    return (
-        <h1>Hello, React.</h1>
-    );
-}
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <h1>Nuevo pedido:</h1>
+
+        <h2>Datos del cliente:</h2>
+
+        <h2>Datos del pedido:</h2>
+
+        <Row>
+          <Col lg={4} md={8}>
+            <Button variant="primary">Cancelar</Button>
+          </Col>
+          <Col lg={8} md={4}>
+            <Button variant="primary">Guardar pedido</Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
