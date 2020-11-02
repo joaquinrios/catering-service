0e3c47755b760f951e55117b9340471c7389c4cd
rtperezyera <tllezroberto9@gmail.com>
2020-11-02 12:29:11 -0600

    Add dummy orders

--- src/screens/orders.jsx
+++ src/screens/orders.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Accordion, Card } from 'react-bootstrap';
 
 import { Navbar } from '../components/navbar';
 
@@ -14,8 +14,58 @@ export const Orders = (props) => {
           </Col>
           <Col lg={8} md={4}>
             <p>
-              <Button variant='primary'>Nuevo Pedido</Button>
+              <Button variant="primary">Nuevo Pedido</Button>
             </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <Row>
+                    <Col>
+                      <h4>Pedido 1</h4>
+                      <p>esto es un pedido</p>
+                      <p>y aquí hay algo de información at-a-glance</p>
+                    </Col>
+                    <Col>
+                      <p>Oct 11</p>
+                      <p>7:00 PM</p>
+                    </Col>
+                  </Row>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    <p>más detalles...</p>
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>Informacion p1</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <Row>
+                    <Col>
+                      <h4>Pedido 2</h4>
+                      <p>esto es un pedido</p>
+                      <p>y aquí hay algo de información at-a-glance</p>
+                    </Col>
+                    <Col>
+                      <p>Oct 12</p>
+                      <p>2:00 PM</p>
+                    </Col>
+                  </Row>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    <p>más detalles...</p>
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>Informacion p2</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
           </Col>
         </Row>
       </Container>
