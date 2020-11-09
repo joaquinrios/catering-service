9c01938edbd205148d785168d8204764950a1c73
rtperezyera <tllezroberto9@gmail.com>
2020-11-08 23:39:31 -0600

    Add basic scaffolding for footer

--- src/screens/landing_page.jsx
+++ src/screens/landing_page.jsx
@@ -11,14 +11,32 @@ export const LandingPage = () => {
           <h1>Slogan e imagen</h1>
           <hr />
           <h1>Servicios</h1>
-          <hr/>
+          <hr />
           <h1>Descuentos</h1>
-          <hr/>
+          <hr />
           <h1>Nosotros</h1>
-          <hr/>
+          <hr />
           <h1>Contacto</h1>
-          <hr/>
-          <h2>Maybe some footer?</h2>
+          <hr />
+          <Row>
+            <Col>
+              <p>footer content</p>
+            </Col>
+            <Col>
+              <p>moar footer content</p>
+            </Col>
+            <Col>
+              <p>even moar footer stuff</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h6>something</h6>
+            </Col>
+            <Col>
+              <h6>something else</h6>
+            </Col>
+          </Row>
         </Container>
       </>
     );
