f9bee11511acdcc9887c366c1b708e8e76499819
Duncanista <30836115+duncanista@users.noreply.github.com>
2020-11-16 18:32:55 -0600

    Add the option to add N products to an order

--- src/screens/new_order.jsx
+++ src/screens/new_order.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
-import { Form as FinalForm, Field as FinalFormField } from 'react-final-form';
+import { Form as FinalForm, Field as FinalFormField,  } from 'react-final-form';
+import { FieldArray as FinalFormFieldArray } from 'react-final-form-arrays';
+import arrayMutators from 'final-form-arrays'
 import Toggle from 'react-toggle';
 import Autosuggest from 'react-autosuggest';
 
@@ -34,166 +36,173 @@ export const NewOrder = ({props}) => {
       <Container>
         <h1>Nuevo pedido</h1>
         <hr/>
-        <Row>
-          
-          <FinalForm onSubmit={onSubmitForm} initialValues={{ newUser: true }}>
-            {({ handleSubmit, submitting, values }) => (
-              <Form>
-                <Col lg={8}>
-                  <h2>Información del evento</h2>
-                  <Form.Group>
-                    <Form.Label>Fecha</Form.Label>
-                    <FinalFormField name='date'>
-                      {({ input }) => <Form.Control {...input} type='date' size='lg'/>}
-                    </FinalFormField>
-                  </Form.Group>
-
-                  <Form.Group>
-                    <Form.Label>Hora de llegada</Form.Label>
-                    <FinalFormField name='time'>
-                      {({ input }) => <Form.Control {...input} type='time' size='lg'/>}
-                    </FinalFormField>
-                  </Form.Group>
-
-                  <Form.Group>
-                    <Form.Label>Personas</Form.Label>
-                    <FinalFormField name='persons'>
-                      {({ input }) => <Form.Control {...input} type='number' size='lg' />}
-                    </FinalFormField>
-                  </Form.Group>
-                </Col>
-
-                <Col lg={8}>
-                  <h2>Cliente</h2>
-                  <Form.Group>
-                    <Form.Label>¿Es nuevo cliente?</Form.Label>
-                    <FinalFormField name='newUser' type='checkbox'>
-                      {({ input }) => <Row><Col><Toggle {...input} /></Col></Row>}
-                    </FinalFormField>
-                  </Form.Group>
-                  
-                  { values.newUser ? (<>
-                    <Form.Group>
-                      <Form.Label>Nombre</Form.Label>
-                      <FinalFormField name='customerName'>
-                        {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
-                      </FinalFormField>
-                    </Form.Group>
-                    <Form.Group>
-                      <Form.Label>Apellidos</Form.Label>
-                      <FinalFormField name='customerName'>
-                        {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
-                      </FinalFormField>
-                    </Form.Group>
-                    <Form.Group>
-                      <Form.Label>Teléfono</Form.Label>
-                      <FinalFormField name='phone'>
-                        {({ input }) => <Form.Control {...input} type='text' size='lg' />}
-                      </FinalFormField>
-                    </Form.Group>
-                    <Form.Group>
-                      <Form.Label>Correo electrónico</Form.Label>
-                      <FinalFormField name='email'>
-                        {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
-                      </FinalFormField>
-                    </Form.Group>
-                    <Form.Group>
-                      <Form.Label>Dirección</Form.Label>
-                      <FinalFormField name='address'>
-                        {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
-                      </FinalFormField>
-                    </Form.Group>
-                  </>) : (<>
-                    <Form.Group>
-                      <Form.Label>Cliente</Form.Label>
-                      <FinalFormField name='customer'>
-                        {({ input }) => <>
-                        <Autosuggest
-                        suggestions={suggestions} 
-                        onSuggestionsFetchRequested={onSuggestionsFetchRequested} 
-                        onSuggestionsClearRequested={onSuggestionsClearRequested} 
-                        getSuggestionValue={getSuggestionValue}
-                        renderSuggestion={renderSuggestion}
-                        onSuggestionSelected={(_, { suggestionValue }) => {
-                          input.onChange(suggestionValue);
-                        }}
-                        inputProps={{name: input.name, value: input.value, onChange: input.onChange, className: 'form-control-lg form-control', placeholder: 'Escriba el nombre del cliente...'}}/>
         
-                        </>}
-                      </FinalFormField>
-                    </Form.Group>
-                  </>)}
-                  
-                  <Form.Group>
-                    <Form.Label>Evento:</Form.Label>
-                    <FinalFormField name='eventName'>
-                      {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
-                    </FinalFormField>
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>¿El evento es recurrente?</Form.Label>
-                    <FinalFormField name='frequent' type='checkbox'>
-                      {({ input }) => <Row><Col><Toggle {...input} /></Col></Row>}
-                    </FinalFormField>
-                    
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Notas:</Form.Label>
-                    <FinalFormField name='eventNotes'>
-                      {({ input }) => <Form.Control {...input} type='textarea' size='lg'/>}
-                    </FinalFormField>
-                  </Form.Group>
-                </Col>
-
-                
-                <h2>Datos del pedido</h2>
+          
+          <FinalForm onSubmit={onSubmitForm} initialValues={{ newUser: true, products: [undefined] }} mutators={{ ...arrayMutators }}>
+            {({ handleSubmit, submitting, values, form }) => (
+              <Form>
                 <Row>
-                  <Col>
-                    <p>[autosuggest platillo]</p>
-                  </Col>
-                  <Col>
-                    <p>[toggle kg, lt, unidades]</p>
-                  </Col>
-                  <Col>
-                    <p>[precio (calculated programatically)]</p>
-                  </Col>
-                  <Col>
-                    <p>boton borrar item</p>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <h3>Total:</h3>
-                  </Col>
-                  <Col>
-                    <h3>$00.00 (calculated)</h3>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
+                  <Col lg={12}><h2>Información del evento</h2></Col>
+                  <Col lg={7}>
                     <Form.Group>
-                      <Form.Label>Indicaciones adicionales:</Form.Label>
-                      <FinalFormField name='orderNotes'>
-                        {({ input }) => (
-                          <Form.Control {...input} type='textarea' />
-                        )}
+                      <Form.Label>Evento</Form.Label>
+                      <FinalFormField name='eventName'>
+                        {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
+                      </FinalFormField>
+                    </Form.Group>
+
+                    <Form.Group>
+                      <Form.Label>Fecha</Form.Label>
+                      <FinalFormField name='date'>
+                        {({ input }) => <Form.Control {...input} type='date' size='lg'/>}
+                      </FinalFormField>
+                    </Form.Group>
+
+                    <Form.Group>
+                      <Form.Label>Hora de llegada</Form.Label>
+                      <FinalFormField name='time'>
+                        {({ input }) => <Form.Control {...input} type='time' size='lg'/>}
                       </FinalFormField>
                     </Form.Group>
                   </Col>
-                </Row>
 
-                <Row>
-                  <Col lg={4} md={8}>
-                    <Button variant='primary'>Cancelar</Button>
+                  <Col lg={5}>
+                    <Form.Group>
+                      <Form.Label>Personas</Form.Label>
+                      <FinalFormField name='persons'>
+                        {({ input }) => <Form.Control {...input} type='number' size='lg' />}
+                      </FinalFormField>
+                    </Form.Group>
+
+                    <Form.Group>
+                      <Form.Label>Notas:</Form.Label>
+                      <FinalFormField name='eventNotes'>
+                        {({ input }) => <Form.Control {...input} type='textarea' size='lg'/>}
+                      </FinalFormField>
+                    </Form.Group>
+
+                    <Form.Group>
+                      <Form.Label>¿El evento es recurrente?</Form.Label>
+                      <FinalFormField name='frequent' type='checkbox'>
+                        {({ input }) => <Row><Col><Toggle {...input} /></Col></Row>}
+                      </FinalFormField>
+                    </Form.Group>
                   </Col>
-                  <Col lg={8} md={4}>
-                    <Button variant='primary' onClick={handleSubmit }>Guardar pedido</Button>
+
+                  <Col lg={12}><h2>Cliente</h2></Col>
+                  
+                  <Col lg={8} className='mb-3'>  
+                    <Form.Group>
+                      <Form.Label>¿Es nuevo cliente?</Form.Label>
+                      <FinalFormField name='newUser' type='checkbox'>
+                        {({ input }) => <Row><Col><Toggle {...input} /></Col></Row>}
+                      </FinalFormField>
+                    </Form.Group>
                   </Col>
+                    
+                    { values.newUser ? (<>
+                    <Col lg={6}>
+                      <Form.Group>
+                        <Form.Label>Nombre</Form.Label>
+                        <FinalFormField name='customerName'>
+                          {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
+                        </FinalFormField>
+                      </Form.Group>
+
+                      <Form.Group>
+                        <Form.Label>Apellidos</Form.Label>
+                        <FinalFormField name='customerName'>
+                          {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
+                        </FinalFormField>
+                      </Form.Group>
+                    </Col>
+                    <Col lg={6}>
+                      <Form.Group>
+                        <Form.Label>Teléfono</Form.Label>
+                        <FinalFormField name='phone'>
+                          {({ input }) => <Form.Control {...input} type='text' size='lg' />}
+                        </FinalFormField>
+                      </Form.Group>
+                      <Form.Group>
+                        <Form.Label>Correo electrónico</Form.Label>
+                        <FinalFormField name='email'>
+                          {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
+                        </FinalFormField>
+                      </Form.Group>
+                      <Form.Group>
+                        <Form.Label>Dirección</Form.Label>
+                        <FinalFormField name='address'>
+                          {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
+                        </FinalFormField>
+                      </Form.Group>
+                    </Col>
+                    </>) : (<>
+                    <Col lg={8}>
+                      <Form.Group>
+                        <Form.Label>Cliente</Form.Label>
+                        <FinalFormField name='customer'>
+                          {({ input }) => <>
+                          <Autosuggest
+                          suggestions={suggestions} 
+                          onSuggestionsFetchRequested={onSuggestionsFetchRequested} 
+                          onSuggestionsClearRequested={onSuggestionsClearRequested} 
+                          getSuggestionValue={getSuggestionValue}
+                          renderSuggestion={renderSuggestion}
+                          onSuggestionSelected={(_, { suggestionValue }) => {
+                            input.onChange(suggestionValue);
+                          }}
+                          inputProps={{name: input.name, value: input.value, onChange: input.onChange, className: 'form-control-lg form-control', placeholder: 'Escriba el nombre del cliente...'}}/>
+          
+                          </>}
+                        </FinalFormField>
+                      </Form.Group>
+                      </Col>
+                    </>)}
+
+                  <Col lg={6} className='mt-5'><h2>Datos del pedido</h2></Col>
+                  <Col lg={6} className='align-right mt-5'>
+                    <Button variant='success' onClick={() => form.mutators.push('products', undefined)}>Añadir producto</Button> {' '}
+                    <Button variant='danger' onClick={(() => form.mutators.pop('products'))}>Borrar último producto</Button>
+                  </Col>
+
+                  <FinalFormFieldArray name='products'>
+                    {({ fields }) => fields.map((name, index) => (<>
+                        <Col lg={8}>
+                          <Form.Group>
+                            <Form.Label>Producto</Form.Label>
+                            <FinalFormField name={`${name}.name`}>
+                              {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
+                            </FinalFormField>
+                          </Form.Group>
+                        </Col>
+
+                        <Col lg={4}>
+                          <Form.Group>
+                            <Form.Label>Unidad de medida</Form.Label>
+                            <FinalFormField name={`${name}.measurement`}>
+                              {({ input }) => <Form.Control {...input} type='text' size='lg'/>}
+                            </FinalFormField>
+                          </Form.Group>
+                        </Col>
+                    </>))}
+                  </FinalFormFieldArray>
+
+                  <Col lg={8} className='align-right'>
+                    <h4>Total: </h4>
+                  </Col>
+                  <Col lg={4} className='align-right'>
+                    <h4>$ 1500.00</h4>
+                  </Col>
+
+                  <Col lg={12} className='mb-5 mt-4 align-right'>
+                    <Button variant='primary' size='lg' onClick={handleSubmit}>Guardar pedido</Button>
+                  </Col>
+                
                 </Row>
               </Form>
             )}
           </FinalForm>
-        </Row> 
+        
       </Container>
     </>
   );
