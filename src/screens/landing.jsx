394229804d5cfda55d1ce1144eaa64d39d6ada95
rtperezyera <tllezroberto9@gmail.com>
2020-11-19 18:34:50 -0600

    Add multicarousel and remove carousel from landing

--- src/screens/landing.jsx
+++ src/screens/landing.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Button, Card, Image, Form, Jumbotron, Carousel, Modal } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card, Image, Form, Modal } from 'react-bootstrap';
 import { Form as FinalForm, Field as FinalFormField } from 'react-final-form';
 import { BsArrowRight, BsX } from 'react-icons/bs';
 import axios from 'axios';
 
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 import { Navbar } from '../components/navbar';
-
-
-
 
 const headingImage =
   'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?cs=srgb&dl=pexels-fauxels-3184183.jpg&fm=jpg';
@@ -37,39 +37,49 @@ export const Landing = () => {
       url: 'https://us-central1-firebasics-85a90.cloudfunctions.net/helloWorld',
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json;charset=UTF-8',
       },
       data: values,
     };
-    axios(options).then((response) => {
-      console.log(response);
-      setModalShow(true);
-      // setResponse() ya no va, o sí??
-    }).catch((error) => {
-      if (error.response) {
-        // this the right way to do it?
-        console.log(error.response);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-    });
+    axios(options)
+      .then((response) => {
+        console.log(response);
+        setModalShow(true);
+        // setResponse() ya no va, o sí??
+      })
+      .catch((error) => {
+        if (error.response) {
+          // this the right way to do it?
+          console.log(error.response);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
   };
 
   return (
     <>
       <Navbar />
 
-      <Modal size='lg' centered show={modalShow} onHide={() => setModalShow(false)}>
+      <Modal
+        size="lg"
+        centered
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      >
         <Modal.Header closeButton>
-          <Modal.Title> <h2>Estamos para ayudarte.</h2>  </Modal.Title>
+          <Modal.Title>
+            {' '}
+            <h2>Estamos para ayudarte.</h2>{' '}
+          </Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
@@ -78,15 +88,16 @@ export const Landing = () => {
               <Col lg={5}>
                 <h4>Atención personalizada</h4>
                 <p>Llámanos y nos ponemos de acuerdo en minutos.</p>
-                <h3 className='text-center'>(55) 1283 8823</h3>
+                <h3 className="text-center">(55) 1283 8823</h3>
               </Col>
               <Col lg={7}>
                 <h4>Hacer mi pedido solo.</h4>
                 <p>
                   Ya tienes fecha, lugar, número de personas, y platillos en
-                  mente. <br/>¡Manos a la obra!
+                  mente. <br />
+                  ¡Manos a la obra!
                 </p>
-                <Button variant='success'>
+                <Button variant="success">
                   Vamos&nbsp;&nbsp;
                   <BsArrowRight />
                 </Button>
@@ -95,96 +106,91 @@ export const Landing = () => {
           </Form>
         </Modal.Body>
 
-        <Modal.Footer> <Button variant='secondary' onClick={() => setModalShow(false)}> Cerrar </Button> </Modal.Footer>
+        <Modal.Footer>
+          {' '}
+          <Button variant="secondary" onClick={() => setModalShow(false)}>
+            {' '}
+            Cerrar{' '}
+          </Button>{' '}
+        </Modal.Footer>
       </Modal>
 
       <Container>
-        <h1 className='bold'>¡Siéntete en casa con Cocina Mary!</h1>
-        <hr/>
-          <Card className='text-white'>
-            <Card.Img src={sloganImage} />
-            <Card.ImgOverlay className='cs-card-slogan'>
-              <Card.Title><h1 className='bold'>¡Porque la familia<br/> es lo primero!</h1></Card.Title>
-            </Card.ImgOverlay>
-          </Card>
+        <h1 className="bold">¡Siéntete en casa con Cocina Mary!</h1>
+        <hr />
+        <Card className="text-white">
+          <Card.Img src={sloganImage} />
+          <Card.ImgOverlay className="cs-card-slogan">
+            <Card.Title>
+              <h1 className="bold">
+                ¡Porque la familia
+                <br /> es lo primero!
+              </h1>
+            </Card.Title>
+          </Card.ImgOverlay>
+        </Card>
 
-
-        <h1 className='text-center mt-3 mb-3 bold p-3' id='services'>Servicios</h1>
-        <hr/>
+        <h1 className="text-center mt-3 mb-3 bold p-3" id="services">
+          Servicios
+        </h1>
+        <hr />
         <Row>
           <Col lg={10}>
             <p>
-              Aquí te podríamos platicar qué nos hace un servico de banquetería único en la ciudad,
-              pero nosotros siempre nos hemos visto como un integrante más en tu mesa, un ser querido
-              cuya compañía hace cada evento único y especial.<br/>
-              La calidez y la cercanía son los ingredientes básicos desde que empezamos a ponernos de
-              acuerdo contigo hasta que tú y tus familiares o amigos hablan de lo singular que fue
-              esta reunión y la comida cuando llega la hora de despedirse.
+              Aquí te podríamos platicar qué nos hace un servico de banquetería
+              único en la ciudad, pero nosotros siempre nos hemos visto como un
+              integrante más en tu mesa, un ser querido cuya compañía hace cada
+              evento único y especial.
+              <br />
+              La calidez y la cercanía son los ingredientes básicos desde que
+              empezamos a ponernos de acuerdo contigo hasta que tú y tus
+              familiares o amigos hablan de lo singular que fue esta reunión y
+              la comida cuando llega la hora de despedirse.
             </p>
           </Col>
         </Row>
 
         <Row>
           <Col>
-            <Carousel>
-              <Carousel.Item>
-                <Image src={carouselImage1} className='d-block w-100' />
-                <Carousel.Caption>
-                  <h1 className='bold'>Paquetes navideños</h1>
-                  <p>
-                    Porque pocas cosas se comparan con gozar de una buena comida o cena en compañía
-                    de la familia esta época decembrina.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image src={carouselImage2} className='d-block w-100' />
-                <Carousel.Caption>
-                  <h3 className='bold'>Reuniones familiares</h3>
-                  <p>
-                    Muchos o pocos, estaremos más que felices de cocinar para todos.<br/>
-                    Explora o pregunta por las opciones.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image src={carouselImage3} className='d-block w-100' />
-                <Carousel.Caption>
-                  <h3 className='bold'>¿Celebrando aniversario en casa?</h3>
-                  <p>
-                    Elijan la opción de su agrado y permítanos acompañarlos.<br/>
-                    Una grandiosa experiencia sin tener que salir de casa.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
+            <h3>multicarousel goes here</h3>
           </Col>
         </Row>
 
-        <h2 className='mt-5 bold'>Cotización personalizada</h2>
+        <h2 className="mt-5 bold">Cotización personalizada</h2>
         <Row>
           <Col lg={8}>
             <p>
-              Con más de quince años de trayectoria, nos ajustamos a toda clase de evento, prespuesto y paladar.
-              Nuestra satisfacción viene de conocerte y ayudarte a encontrar las mejores opciones para que tú y tus
-              invitados queden deleitados al final de cada comida en la que estemos presentes. ¿Te animas a empezar a explorar?
+              Con más de quince años de trayectoria, nos ajustamos a toda clase
+              de evento, prespuesto y paladar. Nuestra satisfacción viene de
+              conocerte y ayudarte a encontrar las mejores opciones para que tú
+              y tus invitados queden deleitados al final de cada comida en la
+              que estemos presentes. ¿Te animas a empezar a explorar?
             </p>
           </Col>
-          <Col lg={4} className='align-right'>
-            <Button size='lg' onClick={() => setModalShow(true)}> Conocer más &nbsp; <BsArrowRight /> </Button>
+          <Col lg={4} className="align-right">
+            <Button size="lg" onClick={() => setModalShow(true)}>
+              {' '}
+              Conocer más &nbsp; <BsArrowRight />{' '}
+            </Button>
           </Col>
         </Row>
 
-        <h1 className='text-center mt-3 mb-3 bold p-3' id='discounts'>Descuentos</h1>
-        <hr/>
+        <h1 className="text-center mt-3 mb-3 bold p-3" id="discounts">
+          Descuentos
+        </h1>
+        <hr />
         <Row>
           <Col lg={8}>
             <p>
-              Nuestra meta es llevar deleite con nuestro sazón a boca de todos, y sabemos que tú y tus acompañantes
-              son clave para alcanzar más y más personas. Conoce cómo recomendarnos con tus amigos puede traerte
-              beneficios para la siguiente vez que decidas invitarnos a tu evento.
+              Nuestra meta es llevar deleite con nuestro sazón a boca de todos,
+              y sabemos que tú y tus acompañantes son clave para alcanzar más y
+              más personas. Conoce cómo recomendarnos con tus amigos puede
+              traerte beneficios para la siguiente vez que decidas invitarnos a
+              tu evento.
             </p>
-            <Button size='lg'> Ver más&nbsp;&nbsp; <BsArrowRight />
+            <Button size="lg">
+              {' '}
+              Ver más&nbsp;&nbsp; <BsArrowRight />
             </Button>
           </Col>
           <Col>
@@ -192,49 +198,77 @@ export const Landing = () => {
           </Col>
         </Row>
 
-        <h1 className='text-center mt-3 mb-3 bold p-3' id='about-us'>Nosotros</h1>
-        <hr/>
+        <h1 className="text-center mt-3 mb-3 bold p-3" id="about-us">
+          Nosotros
+        </h1>
+        <hr />
         <Row>
           <Col lg={4}>
-            <Card className='text-center no-border'>
-              <Container> <Image src={staffImage1} roundedCircle fluid className='cs-circle-image'/> </Container>
+            <Card className="text-center no-border">
+              <Container>
+                {' '}
+                <Image
+                  src={staffImage1}
+                  roundedCircle
+                  fluid
+                  className="cs-circle-image"
+                />{' '}
+              </Container>
               <Card.Body>
                 <Card.Text>
-                  Nuestra cocina está conformada por el más experimentado personal,
-                  que entiende que una comida sabrosa es el pilar de toda reunión,
-                  en toda ocasión.
+                  Nuestra cocina está conformada por el más experimentado
+                  personal, que entiende que una comida sabrosa es el pilar de
+                  toda reunión, en toda ocasión.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col lg={4}>
-            <Card className='text-center no-border'>
-              <Container> <Image src={staffImage2} roundedCircle fluid className='cs-circle-image'/> </Container>
+            <Card className="text-center no-border">
+              <Container>
+                {' '}
+                <Image
+                  src={staffImage2}
+                  roundedCircle
+                  fluid
+                  className="cs-circle-image"
+                />{' '}
+              </Container>
               <Card.Body>
                 <Card.Text>
-                  Cuando disfrutas de nuestros platillos, estás disfrutando de nuestra
-                  compañía. Desde hace más de quince años, nos hemos posicionado como un
-                  valioso integrante más en tus eventos.
+                  Cuando disfrutas de nuestros platillos, estás disfrutando de
+                  nuestra compañía. Desde hace más de quince años, nos hemos
+                  posicionado como un valioso integrante más en tus eventos.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col lg={4}>
-            <Card className='text-center no-border'>
-              <Container> <Image src={staffImage3} roundedCircle fluid className='cs-circle-image'/> </Container>
+            <Card className="text-center no-border">
+              <Container>
+                {' '}
+                <Image
+                  src={staffImage3}
+                  roundedCircle
+                  fluid
+                  className="cs-circle-image"
+                />{' '}
+              </Container>
               <Card.Body>
                 <Card.Text>
-                  Un equipo de atención completamente orientado a ti, apasionado por entender
-                  tus necesidades y feliz de orientarte para exceder las expectativas de tus
-                  familiares o amigos.
+                  Un equipo de atención completamente orientado a ti, apasionado
+                  por entender tus necesidades y feliz de orientarte para
+                  exceder las expectativas de tus familiares o amigos.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
         </Row>
-        
-        <h1 className='text-center mt-3 mb-3 bold p-3' id='contact'>Contacto</h1>
-        <hr/>
+
+        <h1 className="text-center mt-3 mb-3 bold p-3" id="contact">
+          Contacto
+        </h1>
+        <hr />
         <Row>
           <Col lg={5}>
             <h3>¿Quieres hacer un pedido?</h3>
@@ -244,38 +278,45 @@ export const Landing = () => {
                 <Form>
                   <Form.Group>
                     <Form.Label>Nombre</Form.Label>
-                    <FinalFormField name='customerName'>
+                    <FinalFormField name="customerName">
                       {({ input }) => (
-                        <Form.Control {...input} type='text' size='lg' />
+                        <Form.Control {...input} type="text" size="lg" />
                       )}
                     </FinalFormField>
                   </Form.Group>
                   <Form.Group>
                     <Form.Label>Apellidos</Form.Label>
-                    <FinalFormField name='customerLastName'>
+                    <FinalFormField name="customerLastName">
                       {({ input }) => (
-                        <Form.Control {...input} type='text' size='lg' />
+                        <Form.Control {...input} type="text" size="lg" />
                       )}
                     </FinalFormField>
                   </Form.Group>
                   <Form.Group>
                     <Form.Label>Correo electrónico</Form.Label>
-                    <FinalFormField name='email'>
+                    <FinalFormField name="email">
                       {({ input }) => (
-                        <Form.Control {...input} type='text' size='lg' />
+                        <Form.Control {...input} type="text" size="lg" />
                       )}
                     </FinalFormField>
                   </Form.Group>
                   <Form.Group>
                     <Form.Label>Teléfono</Form.Label>
-                    <FinalFormField name='customerPhone'>
+                    <FinalFormField name="customerPhone">
                       {({ input }) => (
-                        <Form.Control {...input} type='number' size='lg' />
+                        <Form.Control {...input} type="number" size="lg" />
                       )}
                     </FinalFormField>
                   </Form.Group>
 
-                  <Button variant='primary' size='lg' onClick={() => onSubmitForm(values)}> Continuar&nbsp;&nbsp; <BsArrowRight /> </Button>
+                  <Button
+                    variant="primary"
+                    size="lg"
+                    onClick={() => onSubmitForm(values)}
+                  >
+                    {' '}
+                    Continuar&nbsp;&nbsp; <BsArrowRight />{' '}
+                  </Button>
                 </Form>
               )}
             </FinalForm>
@@ -283,15 +324,26 @@ export const Landing = () => {
 
           <Col lg={7}>
             <h3>Ubicación</h3>
-            <><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.446371148455!2d-99.14845794847503!3d19.43631244553169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8d59ac21459%3A0x90b4c056728a77cf!2sCalle%20Dr%20Mora%209%2C%20Colonia%20Centro%2C%20Centro%2C%20Cuauht%C3%A9moc%2C%2006000%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1605569892526!5m2!1ses!2smx" width='100%' height='70%' frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></>
+            <>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.446371148455!2d-99.14845794847503!3d19.43631244553169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8d59ac21459%3A0x90b4c056728a77cf!2sCalle%20Dr%20Mora%209%2C%20Colonia%20Centro%2C%20Centro%2C%20Cuauht%C3%A9moc%2C%2006000%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1605569892526!5m2!1ses!2smx"
+                width="100%"
+                height="70%"
+                frameborder="0"
+                allowfullscreen=""
+                aria-hidden="false"
+                tabindex="0"
+              ></iframe>
+            </>
           </Col>
         </Row>
         <hr />
-        <Row className='pt-1 pb-5'>
-          <Col className='bold'>Cocina Mary © 2020</Col>
-          <Col className='bold align-right'>Dr Mora 9, Centro, Cuauhtemoc, 06000, Ciudad de México. </Col>
+        <Row className="pt-1 pb-5">
+          <Col className="bold">Cocina Mary © 2020</Col>
+          <Col className="bold align-right">
+            Dr Mora 9, Centro, Cuauhtemoc, 06000, Ciudad de México.{' '}
+          </Col>
         </Row>
-        
       </Container>
     </>
   );
