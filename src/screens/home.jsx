4d5666299f2213e47dbcba5a6920bc99304e58aa
luisalcantaralo <alcantaraluisalfonso@gmail.com>
2020-11-22 21:43:31 -0600

    Dashboard updated, add events from API to Big Calendar, added current events per month, day and week

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import moment from 'moment';
-import events from '../assets/events'
+import axios from 'axios';
 
 import { Navbar } from '../components/navbar';
 
@@ -12,9 +12,70 @@ require('moment/locale/es.js');
 
 const localizer = momentLocalizer(moment);
 const allViews = Object.keys(Views).map(k => Views[k]);
+const eventsCalendar = [];
+const eventsThisWeek = [];
+const eventsThisMonth = [];
+const eventsToday = [];
 
-export const Home = () => {
-  return (
+
+
+Date.prototype.getWeek = function() {
+  var onejan = new Date(this.getFullYear(),0,1);
+  var today = new Date(this.getFullYear(),this.getMonth(),this.getDate());
+  var dayOfYear = ((today - onejan + 86400000)/86400000);
+  return Math.ceil(dayOfYear/7)
+};
+
+const options = {
+  url: '/api/order_info/',
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8',
+  },
+};
+
+export const Home = (props) => {
+  const [ready, setReady] = useState(false);
+  const [orders, setEvents] = useState(null);
+  useEffect(() => {
+    axios(options).then(response => {
+      const currentWeek = new Date(Date.now()).getWeek();
+      const currentMonth = new Date(Date.now()).getMonth();
+      const today = new Date();
+      const events = response.data.message;
+      events.forEach(event => {
+        var startTime = new Date(event.order_date.replace(' ', 'T'));
+        var endTime = new Date(startTime.getTime());
+        endTime.setHours(endTime.getHours() + 4)
+        var e = {
+          id: 0,
+          title: event.order_event,
+          start: startTime,
+          end: endTime,
+          notes: event.order_notes,
+        }
+        if (startTime.getWeek() == currentWeek && today.getFullYear() == startTime.getFullYear()){
+          eventsThisWeek.push(e);
+        }
+        if (startTime.getMonth() == currentMonth && today.getFullYear() == startTime.getFullYear()){
+          eventsThisMonth.push(e);
+        }
+        if (startTime.getDay() == today.getDay() && startTime.getWeek() == currentWeek && today.getFullYear() == startTime.getFullYear()){
+          eventsToday.push(e);
+        }
+        eventsCalendar.push(e);
+      });
+      setEvents(events)
+      setReady(true);
+      console.log(eventsCalendar);
+    }).catch(error => {
+      console.log(error);
+      setReady(true);
+    });    
+  }, [])
+
+  return ready && (
     <>
       <Navbar/>
       <Container>
@@ -30,11 +91,12 @@ export const Home = () => {
         <Row>
           <Col lg={9} className='cs-calendar'>
             <Calendar
-              onNavigate={(date) => console.log(date)}
-              onView={(view) => console.log(view)}
-              events={events}
+              onNavigate={(date) => console.log("Navigate" + date)}
+              onView={(view) => console.log("View " + view)}
+              events={eventsCalendar}
               startAccessor="start"
               endAccessor="end"
+              onSelectEvent={(event, e) => console.log(event)}
               localizer={localizer}
               messages={{
                 next: '>',
@@ -48,7 +110,35 @@ export const Home = () => {
           </Col>
 
           <Col lg={3}>
-          <h4> Esta semana: [TODO]</h4>
+          <h4> Este mes:</h4>
+          {eventsThisMonth && eventsThisMonth.map(function (event, index){
+            return(
+              <div>
+                <h6>Evento {index+1}: {event.title}</h6>
+                <p>Notas: {event.notes}</p>
+              </div>
+            )
+          })}
+
+          <h4> Esta semana:</h4>
+          {eventsThisWeek && eventsThisWeek.map(function (event, index){
+            return(
+              <div>
+                <h6>Evento {index+1}: {event.title}</h6>
+                <p>Notas: {event.notes}</p>
+              </div>
+            )
+          })}
+
+          <h4> Hoy:</h4>
+          {eventsToday && eventsToday.map(function (event, index){
+            return(
+              <div>
+                <h6>Evento {index+1}: {event.title}</h6>
+                <p>Notas: {event.notes}</p>
+              </div>
+            )
+          })}
           </Col>
         </Row>
       </Container>
